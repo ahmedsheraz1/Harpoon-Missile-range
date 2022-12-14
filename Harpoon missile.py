fbd95db7bb8bf9e68d87c7f4c7815dcd2711a5d3
Ahmed Sheraz <ahmedsheraz1@gmail.com>
2022-12-14 07:53:22 +0500

    Create Harpoon missile.py

---
+++ Harpoon missile.py
@@ -0,0 +1,5 @@
+Python 3.10.2 (v3.10.2:a58ebcc701, Jan 13 2022, 14:50:16) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+from hashlib import blake2b
+blake2b(b'Harpoon missile').hexdigest()
+'e863a9a46a9c0e8eaa7c95f570837de889efdf8afd9ddefc393bfec2d08528f4f3d282465bbb9d8676c7c34f97c627efa7a624b93c20b564867bf1c57feb9e65'
